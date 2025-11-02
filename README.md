b3ee8db599ded217ebe20b19ceb5fe072c0dcf73
Emmanuele Levi <ignisvictorious@gmail.com>
2025-11-02 23:27:18 +0000

    Basic app - All working

---
+++ README.md
@@ -0,0 +1,4 @@
+Freely adapted for educational purposes from:
+
+[¿Quieres hacer APIs PROFESIONALES? Empieza con FastAPI HOY   - Automatiza tu Vida 57.2K subscribers](https://www.youtube.com/watch?v=X7vr3kJs7nM&list=PL7HAy5R0ehQWvo3zU6ymbLPlQ9qU4wo84&index=1)
+
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-# 
+#
 #  Import LIBRARIES
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+
 #  Import FILES
 #  ...
 
 app = FastAPI(title="Mi Primera API", description="Aprendiendo FastAPI", version="0.1.0")
 
-@app.get("/")
-async def root() :
+
+@app.get(path="/")
+async def root() -> dict[str, str]:
     return {"message": "¡Hola, FastAPI!"}
 
 
-# Para ejecutar: uvicorn main:app --reload
-
-
+# Para ejecutar: uvicorn main:app --reload .... Old way!
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
 
 
-# 
+#
 #  Import LIBRARIES
 #  Import FILES
 #  ...
